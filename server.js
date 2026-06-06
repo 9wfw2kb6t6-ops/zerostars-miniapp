a5e4d91de0a2a771a1a8fc986900cb4cfb881870
9wfw2kb6t6-ops <9wfw2kb6t6@privaterelay.appleid.com>
2026-06-06 17:11:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const express = require('express');
+const path = require('path');
 
-const express=require('express');
-const app=express();
-app.get('/api/profile',(req,res)=>{
- res.json({stars:10,energy:99,level:1});
+const app = express();
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.listen(3001);
+
+app.get('/api/profile', (req, res) => {
+res.json({
+stars: 10,
+energy: 99,
+level: 1
+});
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+});
+
