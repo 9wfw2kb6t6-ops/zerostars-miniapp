0b96018c7dbbcb7c239b946a5bbf1ec029286e1d
9wfw2kb6t6-ops <9wfw2kb6t6@privaterelay.appleid.com>
2026-06-06 05:41:30 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+            'ZeroStars',
+            style: TextStyle(
+              color: Colors.amber,
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
